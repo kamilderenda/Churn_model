b9a1458bca20c21ecab9932653e022f6087cf57c
Kamil Derenda <kamilderenda@outlook.com>
2026-03-14 23:14:26 +0100

    adding simple UI and FastApi Endpoint. All is working

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
+import gradio as gr
 import pandas as pd
+from src.serving.inference import predict
 
-app = FastAPI(title='Churn Prediction API')
 
-model=joblib.load('model/Churn_model.pkl')
+app = FastAPI(title='Churn Prediction API',
+                  description='API for predicting customer churn using a pre-trained model.',
+                  version='1.0.0')
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Churn Prediction API!"}
 
 class ChurnRequest(BaseModel):
     CreditScore: int
@@ -24,13 +31,80 @@ class ChurnRequest(BaseModel):
     AgeGroup: object
     CreditScoreClass: object
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Churn Prediction API!"}
 
 @app.post("/predict")
 def predict_churn(data: ChurnRequest):
-    input_df= pd.DataFrame([data.dict()])
-    prediction = model.predict(input_df)[0]
-    probability = model.predict_proba(input_df)[0][1]
-    return {"churn_prediction": int(prediction), "churn_probability": float(probability)}
+    try:
+        prediction = predict(data.dict())
+        return {"churn_prediction": prediction}
+    except Exception as e:
+        return {"error": str(e)}
+
+def gradio_interface(
+    gender, Partner, Dependents, PhoneService, MultipleLines,
+    InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,
+    TechSupport, StreamingTV, StreamingMovies, Contract,
+    PaperlessBilling, PaymentMethod, tenure, MonthlyCharges, TotalCharges
+):
+    payload = {
+        "gender": gender,
+        "Partner": Partner,
+        "Dependents": Dependents,
+        "PhoneService": PhoneService,
+        "MultipleLines": MultipleLines,
+        "InternetService": InternetService,
+        "OnlineSecurity": OnlineSecurity,
+        "OnlineBackup": OnlineBackup,
+        "DeviceProtection": DeviceProtection,
+        "TechSupport": TechSupport,
+        "StreamingTV": StreamingTV,
+        "StreamingMovies": StreamingMovies,
+        "Contract": Contract,
+        "PaperlessBilling": PaperlessBilling,
+        "PaymentMethod": PaymentMethod,
+        "tenure": int(tenure),
+        "MonthlyCharges": float(MonthlyCharges),
+        "TotalCharges": float(TotalCharges),
+    }
+    out = predict(payload)
+    return str(out)
+
+def gradio_interface(
+        geography, gender, credit_score, age, tenure, balance, num_of_products,
+        has_credit_card, is_active_member, estimated_salary
+):
+    payload = {
+        "Geography": geography,
+        "Gender": gender,
+        "CreditScore": int(credit_score),
+        "Age": int(age),
+        "Tenure": int(tenure),
+        "Balance": float(balance),
+        "NumOfProducts": int(num_of_products),
+        "HasCrCard": int(has_credit_card),
+        "IsActiveMember": int(is_active_member),
+        "EstimatedSalary": float(estimated_salary)
+    }
+    out= predict(payload)
+    return str(out)
+
+demo = gr.Interface(
+    fn=gradio_interface,
+    inputs=[
+        gr.Dropdown(['France', 'Spain', 'Germany'], label="Geography"),
+        gr.Dropdown(['Male', 'Female'], label='Gender'),
+        gr.Number(label='Credit Score'),
+        gr.Number(label='Age'),
+        gr.Number(label='Tenure'),
+        gr.Number(label='Balance'),
+        gr.Number(label='Num of Products'),
+        gr.Dropdown([0, 1], label='Has Credit Card'),
+        gr.Dropdown([0, 1], label='Is Active Member'),
+        gr.Number(label='Estimated Salary')
+    ],
+    outputs="text",
+    title="Churn Prediction",
+    description="Fill in the customer details to get a churn prediction.",
+)
+
+app = gr.mount_gradio_app(app, demo, path="/ui")
---
+++ src/serving/inference.py
@@ -0,0 +1,27 @@
+import mlflow
+import pandas as pd
+from pathlib import Path
+import os
+from src.features.build_features import feature_engineering
+
+models_dir = Path("scripts/mlruns/0/models")
+latest = max(models_dir.iterdir(), key=os.path.getmtime)
+model_path = latest / "artifacts"
+
+try:
+    model = mlflow.pyfunc.load_model(str(model_path))
+    print("Model loaded successfully.")
+except Exception as e:
+    print(f"An error occurred while loading the model: {e}")
+
+def predict(data: dict) -> str:
+    try:
+        input_data = feature_engineering(pd.DataFrame([data]))
+        prediction = model.predict(input_data)
+        if prediction[0] == 1:
+            return "The customer is likely to churn."
+        else:
+            return "The customer is unlikely to churn."
+    except Exception as e:
+        print(f"An error occurred during prediction: {e}")
+        return -1
